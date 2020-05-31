70276689bef16260a785f8d0ef0277f561b821bd
Yuaylong <50371003+Yuaylong@users.noreply.github.com>
2020-05-31 19:14:47 +0700

    change Chart code

--- index/Chart.js
+++ index/Chart.js
@@ -1,33 +1,21 @@
-var ctxR = document.getElementById("radarChart").getContext('2d');
-var myRadarChart = new Chart(ctxR, {
-type: 'radar',
+var ctxBc = document.getElementById('bubbleChart').getContext('2d');
+var bubbleChart = new Chart(ctxBc, {
+type: 'bubble',
 data: {
-labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
 datasets: [{
-label: "My First dataset",
-data: [65, 59, 90, 81, 56, 55, 40],
-backgroundColor: [
-'rgba(105, 0, 132, .2)',
-],
-borderColor: [
-'rgba(200, 99, 132, .7)',
-],
-borderWidth: 2
-},
-{
-label: "My Second dataset",
-data: [28, 48, 40, 19, 96, 27, 100],
-backgroundColor: [
-'rgba(0, 250, 220, .2)',
-],
-borderColor: [
-'rgba(0, 213, 132, .7)',
-],
-borderWidth: 2
-}
-]
-},
-options: {
-responsive: true
-}
-});
+label: 'John',
+data: [{
+x: 3,
+y: 7,
+r: 10
+}],
+backgroundColor: "#ff6384",
+hoverBackgroundColor: "#ff6384"
+}, {
+label: 'Peter',
+data: [{
+x: 5,
+y: 7,
+r: 10
+}],
+backgroundColor: "#44e4ee",
